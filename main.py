55d524b266daa88170ede902d9d38b19c8892cab
rithins36 <84023344+rithins36@users.noreply.github.com>
2021-08-14 10:44:26 +0530

    Updated main.py with menu driven program

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
 #Armstrong Number
 num=int(input("Enter a number: "))
+while True:
+  print("\n1.Prime or not")
+  print("2.Armstrong")
+  print("3.Odd or even")
+  print("4.Factorial")
+  print("5.Exit")
 
-print("1.Prime or not \n2.Armstrong \n3.Odd or Even \n4.Factorial")
-choice=int(input("Select Operation : "))
+  choice=int(input("Select Operation : "))
 
-if choice==1:
-  if num<=1:
+  if choice==1:
+    if num<=1:
       print(num," is not a prime number")
-  else:
-    for i in range(2,num):
-      if(num%i)==0:
-        print(num,"is not a prime number")
-        break
     else:
-        print(num,"is a prime number")
+      for i in range(2,num):
+        if(num%i)==0:
+          print(num,"is not a prime number")
+          break
+      else:
+          print(num,"is a prime number")
   
-elif choice==2:
+  elif choice==2:
     
     sum=0
     temp=num
@@ -30,26 +35,29 @@ elif choice==2:
     else:
       print(num,"is not Armstrong number")
 
-elif choice==3:
+  elif choice==3:
 
-  if (num%2)==0:
-    print(num,"is even")
+    if (num%2)==0:
+      print(num,"is even")
+    else:
+      print(num,"is odd")
+
+  elif choice==4:
+    result=1
+    if num<0:
+      print("Invalid")
+    else:
+      for i in range(num,0,-1):
+        result = result*i
+      print(f"Factorial of {num} is: {result}")
+
+
+  elif choice==5:
+    print("Exiting..")
+    break
+
   else:
-    print(num,"is odd")
-
-elif choice==4:
-  result=1
-  if num<0:
-    print("Invalid")
-  else:
-    for i in range(num,0,-1):
-      result = result*i
-    print(f"Factorial of {num} is: {result}")
-
-
-else:
-    print("Wrong choice")
-
+   print("Wrong choice")
 
   
 
